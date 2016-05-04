d46505460b55d7e6097bf4a509a64be92948c040
kokoChris <kokolios1986@yahoo.gr>
2016-05-04 20:42:38 +0300

    add working app.js

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ var express = require('express');
 
 var app = express();
 
-var port = process.env.PORT || 3000,
+var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-    res.redirect('/polls');
+    res.send('giwnia');
 });
 
 
