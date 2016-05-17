84ca8bcfcb6ebfda7d9d428779847e1426cc08bb
kokoChris <kokolios1986@yahoo.gr>
2016-05-17 20:28:08 +0300

    minor

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
 
- request('https://api.foursquare.com/v2/venues/search?client_id=3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5&client_secret=KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG&v=20130815&ll=40.7,-74&query=nightlife',function(error,response,body){
+ request('https://api.foursquare.com/v2/venues/search?client_id=3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5&client_secret=KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG&v=20130815&near=Athens&query=bar&limit=10',function(error,response,body){
  	if(!error && response.statusCode ===200){
  		var parsedBody = JSON.parse(body);
 
