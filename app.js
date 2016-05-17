d0c3b4ac371d957e4567f1e33c4d5c9ce9d8ead8
kokoChris <kokolios1986@yahoo.gr>
2016-05-17 20:05:33 +0300

    first request with foursquare

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
 var express = require('express');
-
+var request = require('request');
 var app = express();
 
 var port = process.env.PORT || 3000;
 
 app.get('/',function(req,res){
-    res.send('giwnia');
+
+ request('https://api.foursquare.com/v2/venues/search?client_id=3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5&client_secret=KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG&v=20130815&ll=40.7,-74&query=nightlife',function(error,response,body){
+ 	if(!error && response.statusCode ===200){
+ 		var parsedBody = JSON.parse(body);
+
+ 		res.send(parsedBody);
+ 	}else{
+
+ 		console.log(error,response.statusCode);	
+ 		res.send('bales');
+ 	}
+ });
 });
 
 
