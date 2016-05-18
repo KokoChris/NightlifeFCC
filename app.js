fe5cdf940c8201dcad05881b8b56fe3c99eb44a4
Chris Kokolios <kokolios1986@yahoo.gr>
2016-05-18 19:48:54 +0300

    create venues

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ app.get('/', function(req, res) {
     request('https://api.foursquare.com/v2/venues/search?client_id=3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5&client_secret=KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG&v=20130815&near=Athens&query=bar&limit=10', function(error, response, body) {
         if (!error && response.statusCode === 200) {
             var parsedBody = JSON.parse(body);
-
-            res.render('landing');
+            var venues = parsedBody.response.venues;
+            res.render('landing',{venues:venues});
         } else {
 
             console.log(error, response.statusCode);
