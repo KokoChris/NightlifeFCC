e33c4f11a6aefb769612459e25251a8cf65237c4
Chris Kokolios <kokolios1986@yahoo.gr>
2016-05-22 00:02:54 +0300

    search functionality and related view

--- app.js
+++ app.js
@@ -8,28 +8,63 @@ app.use(express.static(__dirname + '/public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.set('view engine', 'ejs')
-app.use(function (req,res,next) {
-    
+app.use(function(req, res, next) {
+
     res.locals.currentUser = req.user;
     next();
 });
 
 
+var client_id = '3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5';
+var client_secret = 'KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG';
+var options = {
+    uri: 'https://api.foursquare.com/v2/venues/explore',
+    qs: {
+        client_id: client_id,
+        client_secret: client_secret,
+        v: 20130815,
+        near: 'Athens',
+        query: 'bar',
+        limit: 10 ,
+        venuePhotos:1
+    }
+}
+
+
+
 app.get('/', function(req, res) {
 
-    request('https://api.foursquare.com/v2/venues/search?client_id=3RFULQTUSF5EOKZFGFRF2WNCC5LBUJW2D55HIVROUAYZXNB5&client_secret=KN3RBJGKKAGZITCWN3R1UV2QL43JJ1AONKBF2YH5XGEDMAZG&v=20130815&near=Athens&query=bar&limit=10', function(error, response, body) {
-        if (!error && response.statusCode === 200) {
-            var parsedBody = JSON.parse(body);
-            var venues = parsedBody.response.venues;
-            res.render('landing',{venues:venues});
-        } else {
-
-            console.log(error, response.statusCode);
-            res.send('bales');
-        }
-    });
+    res.render('landing')
 });
 
+
+
+app.post('/bars', function(req, res) {
+
+    options.qs.near = req.body.location;
+
+    request(options, function(error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+
+            var parsedBody = JSON.parse(body);
+            
+            var venues = parsedBody.response.groups[0].items;
+           
+            // res.json(venues);
+            res.render('bars/index', { venues: venues, location: capitalize(req.body.location) });
+        } else {
+
+
+            res.send('Something went wrong');
+        }
+    });
+})
+
+
+function capitalize(word) {
+    return word[0].toUpperCase() + word.slice(1);
+}
 
 app.listen(port, function() {
     console.log('server is running on port ' + port);
