8fa73425f0ff1debb797c443eadca1dd1547d87d
Chris Kokolios <kokolios1986@yahoo.gr>
2016-06-04 22:40:40 +0300

    minor changes pre-deploy

--- config/index.js
+++ config/index.js
@@ -1,1 +1,19 @@
-module.exports = require('./development.json');
+if(process.env.NODE_ENV === 'production'){
+  module.exports = {
+    host:process.env.host || "",
+    dbURI: process.env.dbURI,
+    sessionSecret: process.env.sessionSecret,
+    foursquare:{
+    	clientID: process.env.fsClientID,
+    	clientSecret: process.env.fsClientSecret,
+    	callbackURL: process.env.host + "/auth/foursquare/callback",
+    	profileFields:['id','displayName','photos']
+    }
+  }
+
+
+} else {
+
+  module.exports = require('./development.json');
+
+}
