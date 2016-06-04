1020805607f45e4070bf2585a7703184ff1eff32
Chris Kokolios <kokolios1986@yahoo.gr>
2016-06-04 22:54:48 +0300

    fixing errors

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ var config = require('../config');
 var Mongoose = require('mongoose').connect(config.dbURI);
 
 Mongoose.connection.on('error', function(error) {
-    consolel.log("Oups" + error);
+    console.log("Oups" + error);
 })
 var chatUser = new Mongoose.Schema({
     profileId: String,
